b082b9a07c1cfffd8382e17872318e1e086ad643
Panudech Chuangnuktum <MizzKiiPC@gmail.com>
2016-04-30 15:13:40 +0700

    Initial Commit

---
+++ CompanyTests/CompanyTests.swift
@@ -0,0 +1,36 @@
+//
+//  CompanyTests.swift
+//  CompanyTests
+//
+//  Created by Panudech Chuangnuktum on 4/30/2559 BE.
+//  Copyright © 2559 MizzKii. All rights reserved.
+//
+
+import XCTest
+@testable import Company
+
+class CompanyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
